cdfbc893abe260cbfa4c9df9ab2a8a6e3c479f2d
Gloridust <88082356+Gloridust@users.noreply.github.com>
2024-06-06 23:25:15 +0800

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-ADMIN_EMAIL = "admin@example.com"
-SYSTEM_EMAIL = "system@example.com"
-SYSTEM_EMAIL_PASSWORD = "your_password"
-IMAP_SERVER = "imap.example.com"
-SMTP_SERVER = "smtp.example.com"
-SMTP_PORT = 587
