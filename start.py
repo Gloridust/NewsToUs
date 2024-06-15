b88b855031358fcdb89fb0d2fc1700d0f4c64ee1
Gloridust <Gloridust@gmail.com>
2024-06-15 18:49:45 +0800

    Update start.py

--- start.py
+++ start.py
@@ -1,6 +1,10 @@
 import main
 import time
 
+loop_times = 0
 while True:
+    loop_times = loop_times + 1
+    print(">>>loop {looptime} start")
     main.main()
     time.sleep(5)
+    print(">>>loop {looptime} end")
