14b41ff7eb706291a378933d7c79747c124c7f59
Gloridust <Gloridust@gmail.com>
2024-06-18 18:37:49 +0800

    Update start.py

--- start.py
+++ start.py
@@ -1,6 +1,6 @@
 import main
 import time
-from datetime import datetime
+from datetime import datetime, timedelta
 import news_reporter
 import email_handler
 import config
@@ -17,20 +17,17 @@ while True:
     stop_main = time.perf_counter()
     print(f">>>news_reporter start")
     start_news_reporter = time.perf_counter()
-    
+
     current_time = datetime.now()
-    
-    # 如果当前时间是23:59，重置 sent_news_today
-    if current_time.hour == 23 and current_time.minute == 59:
-        sent_news_today = False
-        print("sent_news_today 已重置为 False")
-    
-    # 如果 sent_news_today 为 False 且当前时间是 XX:XX+5 内，执行 news_reporter.main() 并设置 sent_news_today 为 True
-    if (not sent_news_today and 
-    current_time.hour == config.news_send_hour and 
-    config.news_send_min <= current_time.minute < config.news_send_min + 5):
-        output_html=news_reporter.main()
-        subject="NewsToUs:"+datetime.now().strftime('%Y-%m-%d')
+
+    # 配置发送时间和结束时间
+    send_time = current_time.replace(hour=config.news_send_hour, minute=config.news_send_min, second=0, microsecond=0)
+    send_time_end = send_time + timedelta(minutes=5)
+
+    # 检查是否在发送时间范围内
+    if not sent_news_today and send_time <= current_time < send_time_end:
+        output_html = news_reporter.main()
+        subject = "NewsToUs:" + datetime.now().strftime('%Y-%m-%d')
         email_handler.send_newsletter(subject, output_html)
         sent_news_today = True
         print(f"news_reporter.main() 已在 {current_time.strftime('%Y-%m-%d %H:%M:%S')} 执行，sent_news_today 设为 True。")
@@ -38,8 +35,8 @@ while True:
 
     print(f">>>loop {loop_times} end")
     stop_all = time.perf_counter()
-    print("main time:",stop_main - start_all)
-    print("news_reporter time:",stop_all - start_news_reporter)
-    print("All time:",stop_all - start_all)
+    print("main time:", stop_main - start_all)
+    print("news_reporter time:", stop_all - start_news_reporter)
+    print("All time:", stop_all - start_all)
     
     time.sleep(3)
