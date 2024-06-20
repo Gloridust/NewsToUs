c0dbcfcd9a7d3314f56c5b59bd9832c1ee41d609
Gloridust <Gloridust@gmail.com>
2024-06-20 13:07:27 +0800

    Update example.config.py

--- example.config.py
+++ example.config.py
@@ -8,9 +8,11 @@ SMTP_PORT = 587
 
 ADMIN_EMAIL_HOURS_LIMIT = 12
 
+news_send_times = [
+    {"hour": 7, "minute": 00},
+    {"hour": 13, "minute": 5},
+    {"hour": 18, "minute": 00},]
+
 # OpenAI
 chat_url="https://apikeyplus.com/v1/chat/completions" #此处为中转URL，请更换为你自己的URL或官方URL
 APIKEY=""
-
-news_send_hour=7
-news_send_hour=00
